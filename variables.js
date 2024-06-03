bc1fad21ba42f273b073a9d13da3a2073113f6d9
Jimmie Cox <jamescox995@gmail.com>
2024-06-02 22:17:50 -0400

    Finished rest of assignment

--- variables.js
+++ variables.js
@@ -12,6 +12,15 @@ console.log((4 + 6 + 9) / 77);
 
 console.log("Section 4");
 
+let a = 10;
+
 console.log("Section 5");
 
+const MAX = 57;
+let actual = MAX - 13;
+let percentage = actual / MAX;
+
 console.log("Section 6");
+
+
+// All other code is done in the inspect element console in google as per instructions of the Odin Project
