25614e26ec895d788bd8006a6295a81dc4212f8f
Jimmie Cox <jamescox995@gmail.com>
2024-06-02 22:09:56 -0400

    Completed the first half of the assignment

--- variables.js
+++ variables.js
@@ -1,1 +1,17 @@
-console.log("Hello world");
+console.log("Section 1");
+
+console.log(23 + 97);
+
+console.log("Section 2");
+
+console.log(23 + 97 + 42 + 12 + 6 + 17);
+
+console.log("Section 3");
+
+console.log((4 + 6 + 9) / 77);
+
+console.log("Section 4");
+
+console.log("Section 5");
+
+console.log("Section 6");
