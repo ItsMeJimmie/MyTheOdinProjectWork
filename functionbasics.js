8b9fb897e55d753c471a2a584685ce9b84ddf607
Jimmie Cox <jamescox995@gmail.com>
2024-06-06 20:25:58 -0400

    This is where I finished the rest of the functions

--- functionbasics.js
+++ functionbasics.js
@@ -3,13 +3,13 @@ function add7 (number) {
 }
 
 function multiply (number1, number2) {
-
+    return number1 * number2;
 }
 
 function capitalize (word) {
-
+    return word[0].toUpperCase() + word.slice(1).toLowerCase();
 }
 
-function lastLetter() {
-    
+function lastLetter(word) {
+    return word[(word.length) - 1];
 }
